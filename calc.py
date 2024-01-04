3e098d2581a396201dcfefeea31cad175a8b5e51
Naphat Piphattanakarn <naphat.piphattanakarn@gmail.com>
2024-01-03 22:36:15 -0500

    add main function

---
+++ calc.py
@@ -0,0 +1,16 @@
+def main():
+    "Calculator program"
+    x = input('Please type the firt value: ')
+    y = input('Please type the second value: ')
+
+    # TODO: ask user to select 1 operation from "+-*/"
+    # then perform based on the operation
+
+    # Perform addition
+    result = x + y
+
+    print('The result of {x} + {y} is {result}')
+
+
+if __name__ == '__main__':
+    main()

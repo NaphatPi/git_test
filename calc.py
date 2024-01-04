cde87e95b1f13ff581ea156ee7426ec3fbc0317e
Naphat Piphattanakarn <naphat.piphattanakarn@gmail.com>
2024-01-03 22:38:56 -0500

    fix value casting

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 def main():
     "Calculator program"
-    x = input('Please type the firt value: ')
-    y = input('Please type the second value: ')
+    x = int(input('Please type the firt value: '))
+    y = int(input('Please type the second value: '))
 
     # TODO: ask user to select 1 operation from "+-*/"
     # then perform based on the operation
@@ -9,7 +9,7 @@ def main():
     # Perform addition
     result = x + y
 
-    print('The result of {x} + {y} is {result}')
+    print(f'The result of {x} + {y} is {result}')
 
 
 if __name__ == '__main__':
